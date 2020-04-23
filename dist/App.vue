bf498483c924053d2130a8ad54bf07f8bef6a571
lock92 <7467422+lock92@user.noreply.gitee.com>
2020-04-23 15:58:25 +0800

    测试

---
+++ dist/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="app-container">
+    <!-- header 头部部分 -->
+    <mt-header fixed title="云购物" class="header-size" style="font-size: 18px;">
+      <router-link to="/" slot="left">
+        <mt-button icon="back"></mt-button>
+      </router-link>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
+
+    <!-- main 主体部分 -->
+    <transition mode="out-in">
+      <router-view></router-view>
+    </transition>
+
+    <!-- footer 底部部分 -->
+    <nav class="mui-bar mui-bar-tab">
+      <router-link class="mui-tab-item-lib" to="/home">
+        <span class="mui-icon mui-icon-home"></span>
+        <span class="mui-tab-label">首页</span>
+      </router-link>
+      <router-link class="mui-tab-item-lib" to="/member">
+        <span class="mui-icon mui-icon-contact">
+          <span class="mui-badge">9</span>
+        </span>
+        <span class="mui-tab-label">会员</span>
+      </router-link>
+      <router-link class="mui-tab-item-lib" to="/shopcar">
+        <span class="mui-icon glyphicon glyphicon-shopping-cart"></span>
+        <span class="mui-tab-label">购物车</span>
+      </router-link>
+      <router-link class="mui-tab-item-lib" to="/search">
+        <span class="mui-icon mui-icon-search"></span>
+        <span class="mui-tab-label">搜索</span>
+      </router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+.app-container {
+  padding-top: 40px;
+  padding-bottom: 50px;
+  overflow-x: hidden;
+}
+.v-enter {
+  opacity: 0;
+  transform: translateX(100%);
+}
+.v-leave-to {
+  opacity: 0;
+  transform: translateX(-100%);
+  position: absolute;
+}
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.8s ease;
+}
+.mui-bar-tab .mui-tab-item-lib.mui-active {
+    color: #007aff;
+}
+.mui-bar-tab .mui-tab-item-lib {
+    display: table-cell;
+    overflow: hidden;
+    width: 1%;
+    height: 50px;
+    text-align: center;
+    vertical-align: middle;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    color: #929292;
+}
+.mui-bar-tab .mui-tab-item-lib .mui-icon {
+    top: 3px;
+    width: 24px;
+    height: 24px;
+    padding-top: 0;
+    padding-bottom: 0;
+}
+.mui-bar-tab .mui-tab-item-lib .mui-icon~.mui-tab-label {
+    font-size: 11px;
+    display: block;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+.header-size {
+  z-index: 99;
+}
+</style>
